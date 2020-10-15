778c9baab1da1916546daf28baab1df98f44575e
Thad Hoskin <thad.hoskin-01@gmail.com>
2020-10-15 16:51:15 +1100

    added another way to add actions

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,14 @@ import "./Button.css";
 export default function Button(props) {
   const { variant = "primary", children, ...rest } = props;
 
+  function handleClick() {
+    console.log("hello this is a click");
+  }
+
   return (
     <div className="button">
-      <button className={`button ${variant}`} {...rest}>
-        {children}
+      <button onClick={handleClick} className={`button ${variant}`} {...rest}>
+        Click Me
       </button>
     </div>
   );
